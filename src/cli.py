d44dca718f6d8739c2c9447fe918898959e50ca3
Vadzim Uyunnik <v.uyunnik@godeltech.com>
2025-07-06 22:18:56 +0200

    add Typer entry-point stub

---
+++ src/cli.py
@@ -0,0 +1,14 @@
+import asyncio
+import typer
+
+app = typer.Typer(no_args_is_help=True)
+
+@app.command()
+def process():
+    asyncio.run(_process_async())
+
+async def _process_async():
+    print("starting invoicer-cli")
+
+if __name__ == "__main__":
+    app()
