019e070a1e4077f82a69883d2f5439b7157bbf20
Vadzim Uyunnik <v.uyunnik@godeltech.com>
2025-07-06 22:47:07 +0200

    add pre-commit hooks for ruff and mypy

--- src/cli.py
+++ src/cli.py
@@ -3,12 +3,15 @@ import typer
 
 app = typer.Typer(no_args_is_help=True)
 
+
 @app.command()
 def process():
     asyncio.run(_process_async())
 
+
 async def _process_async():
     print("starting invoicer-cli")
+
 
 if __name__ == "__main__":
     app()
